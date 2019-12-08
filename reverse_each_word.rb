7234dd7067db05e3af95f69f36aaaaff6fdf8f51
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 17:47:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,12 @@ def reverse_each_word(string)
   result.join(" ")
 end
 
-def reverse_using_collect(string)
+def reverse_each_word(string)
   array = string.split(" ")
   result = []
   array.collect do|string|
     result << string.reversed
   end
-  result.join(" ")  
+  result.join(" ")
 
 end
